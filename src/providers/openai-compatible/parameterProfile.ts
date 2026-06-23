91f6f24fa623560c87e4532c3089dd1c6de67ac4
Max19970 <maxshubert@yandex.ru>
2026-06-24 02:34:09 +0300

    Restore OpenAI-compatible edit parameters

--- src/providers/openai-compatible/parameterProfile.ts
+++ src/providers/openai-compatible/parameterProfile.ts
@@ -2,14 +2,5 @@ import type { ProviderGenerationParamProfile } from '../../entities/generation-p
 
 export const openAiCompatibleGenerationParamProfile = {
   id: 'openai-compatible.default',
-  include: 'all',
-  byMode: {
-    edit: 'all'
-  },
-  isAvailable: ({ mode, definition, current }) => {
-    if (mode === 'edit' && (definition.id === 'generationParam.quality' || definition.id === 'generationParam.moderation')) {
-      return false;
-    }
-    return current;
-  }
+  include: 'all'
 } satisfies ProviderGenerationParamProfile;
